c82a71a9a355db4a9f03eb529e335e5167333668
Adrian Iliesiu <ailiesiu@cisco.com>
2018-06-06 12:29:24 -0700

    Changed running script location

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 
-f = os.popen('mv -f /bootflash/autoconfig/adriani/running.latest /home/guestshell/nxos-workshop/running')
+f = os.popen('mv -f /bootflash/autoconfig/adriani/running.latest /home/vagrant/nxos-workshop/running')
 who = f.read()
 f.close
 print "Result:", who
-f = os.popen('cd /home/guestshell/nxos-workshops; git add running')
+f = os.popen('cd /home/vagrant/nxos-workshops; git add running')
 who = f.read()
 f.close
 print "Result:", who
-f = os.popen('cd /home/guestshell/nxos-workshops; git commit -m "Latest change"')
+f = os.popen('cd /home/vagrant/nxos-workshops; git commit -m "Latest change"')
 who = f.read()
 f.close
 print "Result:", who
-f = os.popen('cd /home/guestshell/nxos-workshops; chvrf management git push')
+f = os.popen('cd /home/vagrant/nxos-workshops; chvrf management git push')
 who = f.read()
 f.close
 print "Result:", who
